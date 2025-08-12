2693019223ac9ef0f1fef44c9e96f3310a7f6aaf
FrightSega <157976357+FrightSega@users.noreply.github.com>
2025-08-12 13:09:39 +0800

    chore: 更新导航数据 - 2025/8/12 13:09:38

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -18,7 +18,7 @@ export const mockData = {
           "name": "豆包",
           "url": "https://www.doubao.com/",
           "description": "",
-          "icon": "https://img.dogged.cn/i/2024/07/02/66840b19d0220.png"
+          "icon": "https://lf-flow-web-cdn.doubao.com/obj/flow-doubao/doubao/web/logo-icon.png"
         },
         {
           "id": "site-1754970271933",
@@ -28,11 +28,39 @@ export const mockData = {
           "icon": "https://raw.githubusercontent.com/mohmoussad/Restore-Twitter-Bird/refs/heads/master/images/logo.ico"
         },
         {
+          "id": "site-1754973030420",
+          "name": "Youtube",
+          "url": "https://www.youtube.com",
+          "description": "",
+          "icon": "/sitelogo/www.youtube.com.ico"
+        },
+        {
           "id": "site-1754971286400",
           "name": "抖音",
           "url": "https://www.douyin.com/",
           "description": "",
           "icon": "https://favicon.run/favicon?domain=www.douyin.com&sz=64"
+        },
+        {
+          "id": "site-1754973666856",
+          "name": "Gmail",
+          "url": "https://mail.google.com",
+          "description": "",
+          "icon": "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico"
+        },
+        {
+          "id": "site-1754973449083",
+          "name": "Hotmail",
+          "url": "https://outlook.live.com",
+          "description": "",
+          "icon": "https://outlook.live.com/mail/favicon.ico"
+        },
+        {
+          "id": "site-1754973258033",
+          "name": "QQ邮箱",
+          "url": "https://mail.qq.com",
+          "description": "",
+          "icon": "https://mail.qq.com/zh_CN/htmledition/images/favicon/qqmail_favicon_96h.png"
         }
       ]
     },
@@ -69,63 +97,133 @@ export const mockData = {
           "url": "https://github.com/features/copilot",
           "description": "",
           "icon": "/sitelogo/github.com.ico"
+        },
+        {
+          "id": "site-1754974075219",
+          "name": "DeepSeek",
+          "url": "https://www.deepseek.com",
+          "description": "",
+          "icon": "https://www.deepseek.com/favicon.ico"
+        },
+        {
+          "id": "site-1754974143427",
+          "name": "豆包",
+          "url": "https://www.doubao.com",
+          "description": "",
+          "icon": "https://lf-flow-web-cdn.doubao.com/obj/flow-doubao/doubao/web/logo-icon.png"
+        },
+        {
+          "id": "site-1754974246608",
+          "name": "腾讯元宝",
+          "url": "https://yuanbao.tencent.com",
+          "description": "",
+          "icon": "https://static.yuanbao.tencent.com/logo-v2.png"
+        },
+        {
+          "id": "site-1754974309689",
+          "name": "文心一言",
+          "url": "https://yiyan.baidu.com",
+          "description": "",
+          "icon": "https://eb-static.cdn.bcebos.com/logo/favicon.ico"
+        },
+        {
+          "id": "site-1754974391391",
+          "name": "讯飞星火",
+          "url": "https://xinghuo.xfyun.cn",
+          "description": "",
+          "icon": "https://xinghuo.xfyun.cn/spark-icon.ico"
+        },
+        {
+          "id": "site-1754974456841",
+          "name": "Kimi",
+          "url": "https://www.kimi.com",
+          "description": "",
+          "icon": "https://statics.moonshot.cn/kimi-web-seo/favicon.ico"
+        },
+        {
+          "id": "site-1754974515300",
+          "name": "秘塔",
+          "url": "https://metaso.cn",
+          "description": "",
+          "icon": "https://metaso.cn/favicon.ico"
+        },
+        {
+          "id": "site-1754974545729",
+          "name": "纳米",
+          "url": "https://bot.n.cn",
+          "description": "",
+          "icon": "https://p1.ssl.qhimg.com/t11098f6bcdb3c1c166a74ad70c.png"
+        },
+        {
+          "id": "site-1754974600580",
+          "name": "天工",
+          "url": "https://www.tiangong.cn",
+          "description": "",
+          "icon": "https://www.plnav.com/api/favicon?url=https://search.tiangong.cn/"
+        },
+        {
+          "id": "site-1754974735092",
+          "name": "Reportify",
+          "url": "https://reportify.cn",
+          "description": "",
+          "icon": "https://reportify.cn/favicon.ico"
+        },
+        {
+          "id": "site-1754974774113",
+          "name": "博查",
+          "url": "https://bochaai.com",
+          "description": "",
+          "icon": "https://bochaai.com/icon.ico?d10c63b34b618305"
+        },
+        {
+          "id": "site-1754974818143",
+          "name": "千问",
+          "url": "https://chat.qwen.ai",
+          "description": "",
+          "icon": "https://assets.alicdn.com/g/qwenweb/qwen-webui-fe/0.0.180/favicon.png"
+        },
+        {
+          "id": "site-1754975031365",
+          "name": "Gemini",
+          "url": "https://gemini.google.com",
+          "description": "",
+          "icon": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Google-gemini-icon.svg/1024px-Google-gemini-icon.svg.png?20240826133250"
+        },
+        {
+          "id": "site-1754975221588",
+          "name": "Grok",
+          "url": "https://grok.com",
+          "description": "",
+          "icon": "https://img.icons8.com/?size=100&id=8QGE7uQedCwQ&format=png&color=000000"
         }
       ]
-    },
-    {
-      "id": "cloud",
-      "name": "云服务",
-      "icon": "☁️",
-      "order": 2,
-      "sites": []
-    },
-    {
-      "id": "dev-tools",
-      "name": "开发工具",
-      "icon": "🛠️",
-      "order": 3,
-      "sites": []
-    },
-    {
-      "id": "community",
-      "name": "社区论坛",
-      "icon": "👥",
-      "order": 6,
-      "sites": []
-    },
-    {
-      "id": "design",
-      "name": "设计工具",
-      "icon": "🎨",
-      "order": 4,
-      "sites": []
-    },
-    {
-      "id": "finance",
-      "name": "财经投资",
-      "icon": "💰",
-      "order": 5,
-      "sites": []
     },
     {
       "id": "learning",
       "name": "学习资源",
       "icon": "📚",
-      "order": 6,
+      "order": 2,
+      "sites": []
+    },
+    {
+      "id": "category-1754975348716",
+      "icon": "🛒",
+      "name": "线上购物",
+      "order": 3,
       "sites": []
     },
     {
       "id": "tools",
       "name": "在线工具",
       "icon": "⚙️",
-      "order": 7,
+      "order": 4,
       "sites": []
     },
     {
       "id": "entertainment",
-      "name": "娱乐休闲",
-      "icon": "🎮",
-      "order": 8,
+      "name": "影音娱乐",
+      "icon": "🎬",
+      "order": 5,
       "sites": [
         {
           "id": "bilibili",
@@ -142,16 +240,7 @@ export const mockData = {
           "icon": "/sitelogo/www.youtube.com.ico"
         }
       ]
-    },
-    {
-      "id": "office",
-      "name": "办公协作",
-      "icon": "💼",
-      "order": 9,
-      "sites": []
     }
   ],
-  "title": "小步导航",
-  "_fileSha": "beb3dacd2d448cd5dbdd1fb2c544a413f5e0bec6",
-  "search": "google"
+  "title": "小步导航"
 }
